74289d7b5e1e185619696e26d83a8c601bd18d26
brikios <mouadh.briki@gmail.com>
2023-05-18 03:44:08 +0100

    chat works v2

--- Backend/controllers/ConversationController.js
+++ Backend/controllers/ConversationController.js
@@ -13,7 +13,18 @@ export const newConversation=async(req,res,next)=>{
         next(err)
     }
 }
-
+export const newConv=async(req,res,next)=>{
+    const newC=new Conversation({
+        members :[req.user.id,req.params.id],
+    })
+    try{
+        const saveConv= await newC.save();
+        res.status(200).json(saveConv)
+    }catch(err)
+    {
+        next(err)
+    }
+}
 export const getConversationByUserId=async(req,res,next)=>{
     try{
         const conversation = await Conversation.find({
--- Backend/routes/conversation.js
+++ Backend/routes/conversation.js
@@ -1,11 +1,11 @@
 import express  from "express";
-import { getConversationByUserId, newConversation } from "../controllers/ConversationController.js";
+import { getConversationByUserId, newConv, newConversation } from "../controllers/ConversationController.js";
 
-
+import { verifyToken, verifyTokenContract, verifyUser } from "../middlewares/verifyToken.js";
 const router = express.Router();
 
 router.post('/newConversation',newConversation );
 
 router.get('/getConvById/:userId',getConversationByUserId);
-
+router.post('/newC/:id',verifyToken,newConv);
 export default router
